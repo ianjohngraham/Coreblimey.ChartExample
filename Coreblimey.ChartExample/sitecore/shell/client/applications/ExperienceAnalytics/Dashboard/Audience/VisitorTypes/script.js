7af3840e37a529dab0eac9d62b2ad1dc97940cd9
ianjohngraham@gmail.com <ianjohngraham@gmail.com>
2015-08-17 21:08:07 +0100

    updated script

--- Coreblimey.ChartExample/sitecore/shell/client/applications/ExperienceAnalytics/Dashboard/Audience/VisitorTypes/script.js
+++ Coreblimey.ChartExample/sitecore/shell/client/applications/ExperienceAnalytics/Dashboard/Audience/VisitorTypes/script.js
@@ -6,21 +6,20 @@ var extendedPage = sc.Definitions.App.extend({
 initialized: function () {
 var app = this;
 
-// Log pice chart errors, handy for debugging..
 app.PieChart1.on("error", function (error) {
 console.log(error);
 });
 
-// Create RequestOptions object..
+
 var requestOptions = {
 url: "/sitecore/api/visitdata"
 , parameters: ""
 , onSuccess: function (data) {
-// We’ve successfully retrieved data from the server!
+
 console.log("this is the data" +data);
 }
 };
-// Make the ChartDataProvider get the data from the server..
+
 app.ChartDataProvider1.viewModel.getData(requestOptions);
 }
 });
